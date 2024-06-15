b7fe315c8051314ae16ff3ee121288878d943339
Bananaasl <166767146+Bananaasl@users.noreply.github.com>
2024-06-15 16:45:38 +0300

    Задание выполнено

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        List<User> users = userService.getAllUsers();
+        users.add(new User("Anar", "Aslanov", (byte)22));
+        users.add(new User("Anastasia", "Borovitskay", (byte)22));
+        users.add(new User("Andrey", "Ivanov", (byte)25));
+        users.add(new User("Ivan", "Ivanov", (byte)21));
+
+        for (User user : users) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println("User с именем – " + user.getName() + " добавлен в базу данных");
+        }
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+
     }
 }
